06986120ec37a187cb3b5995bf94dd093170044e
Jon @ Mandala <jon.morgan@co-perform.com.au>
2026-06-10 19:06:31 +1000

    Auto-sync: 2026-06-10 19:06:31

---
+++ routes.js
@@ -0,0 +1,9 @@
+// routes.js - Private Client Mapping Registry
+// To add a new client, simply append a new key/value row below.
+// Remember to encode your Proton URLs to Base64 using btoa() first.
+
+window.SHIM_PORTAL_ROUTES = {
+    "SHIM-HANSON-26": "aHR0cHM6Ly9kcml2ZS5wcm90b24ubWUvdXJscy9leGFtcGxlLWhhbnNvbi1kcm9w",
+    "SHIM-BORAL-99": "aHR0cHM6Ly9kcml2ZS5wcm90b24ubWUvdXJscy9leGFtcGxlLWJvcmFsLWRyb3A=",
+    "SHIM-SPEEDY-72": "aHR0cHM6Ly9kcml2ZS5wcm90b24ubWUvdXJscy9leGFtcGxlLXNwZWVkeS1kcm9w"
+};
